f56af46bc7ba8a278f22348dc991462285b4134d
KuroLeone <71819415+KuroLeone@users.noreply.github.com>
2021-01-03 18:29:33 +0300

    Update Comment.php

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -1,25 +1,92 @@
 <?php
 
-namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 
-class Comment extends Model
+namespace App\Http\Controllers;
+
+use App\Comment;
+use App\Reply;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class CommentsController extends Controller
 {
-    use HasFactory;
+    // Display a listing of the resource.
+    
+    public function index()
+    {
+        
+    }
 
-    protected $fillable = [ 'id',
-							'post_id',
-							'user_id',
-							'comment'];
+    // Show the form for creating a new resource.
+   
+    public function create()
+    {
+    }
 
-    protected $primarykey = 'id';
+    //Store a newly created resource in storage.
+    
+    public function store(Request $request)
+    {
+        if (Auth::check()) {
+            Comment::create([
+                'name' => Auth::user()->name,
+                'comment' => $request->input('comment'),
+                'user_id' => Auth::user()->id
+            ]);
 
-	public function user(){
-		return $this->belongsTo('App\Models\User');
-	}	
-	public function post(){
-        return $this->belongsTo('App\Models\Post');
+            return redirect()->route('home')->with('success','Comment Added..!');
+        }else{
+            return back()->withInput()->with('error','Something wrong');
+        }
+
+
+        
+    }
+
+    //Display the specified resource.
+   
+    public function show(Comment $comment)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Comment  $comment
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Comment $comment)
+    {
+        //
+    }
+
+    // Update the specified resource in storage.
+   
+    public function update(Request $request, Comment $comment)
+    {
+        
+    }
+
+    // Remove the specified resource from storage.
+    public function destroy(Comment $comment)
+    {
+        if (Auth::check()) {
+
+            $reply = Reply::where(['comment_id'=>$comment->id]);
+            $comment = Comment::where(['user_id'=>Auth::user()->id, 'id'=>$comment->id]);
+            if ($reply->count() > 0 && $comment->count() > 0) {
+                $reply->delete();
+                $comment->delete();
+                return 1;
+            }else if($comment->count() > 0){
+                $comment->delete();
+                return 2;
+            }else{
+                return 3;
+            }
+
+        }    
     }
 }
